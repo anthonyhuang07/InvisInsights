42f80a19cc4fff07a6edbe644de9564d1575df00
Anthony Huang <anthonyhuang07@icloud.com>
2026-01-17 14:41:09 -0500

    frontend work pls?

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,18 @@
 import express from 'express';
 import fetch from 'node-fetch';
 import pkg from 'pg';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
 
 const { Pool } = pkg;
 const app = express();
